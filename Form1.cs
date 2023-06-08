c0a42270f4e263fe8970303f92b6b09264ddedc3
PaulineFR <pauline.franquelin@gmail.com>
2023-06-08 12:59:59 +0200

    draw grid

--- Form1.cs
+++ Form1.cs
@@ -4,20 +4,61 @@ namespace TaquinPuzzle;
 
 public partial class Form1 : Form
 {
-    public Grid _grid { get; set; }
+    private Grid _grid { get; set; }
 
     public Form1()
     {
         InitializeComponent();
     }
 
+    #region Events
+
     private void numericGridSize_ValueChanged(object sender, EventArgs e)
     {
-        numericNbAgents.Maximum = numericGridSize.Value;
+        numericNbAgents.Maximum = numericGridSize.Value * numericGridSize.Value - 1 ;
     }
 
     private void btnRun_Click(object sender, EventArgs e)
     {
         _grid = new Grid((int)numericGridSize.Value, (int)numericNbAgents.Value);
+        drawGrid();
     }
+
+    #endregion
+
+    #region Drawing grid
+
+    private void drawGrid()
+    {
+        panelGrid.Refresh();
+
+        Graphics graph = panelGrid.CreateGraphics();
+        Pen penIs = new Pen(Brushes.Black, 1);
+
+        int lines = (int)numericGridSize.Value;
+        float x = 0;
+        float y = 0;
+        float xSpace = panelGrid.Width / lines;
+        float ySpace = panelGrid.Height / lines;
+
+        // vertical lines
+        for (int i = 0; i < lines + 1; i++)
+        {
+            graph.DrawLine(penIs, x, y, x, panelGrid.Height);
+            x += xSpace;
+
+        }
+
+        // vertical lines
+        x = 0;
+        for (int i = 0; i < lines + 1; i++)
+        {
+            graph.DrawLine(penIs, x, y, panelGrid.Width, y);
+            y += ySpace;
+
+        }
+        
+    }
+
+    #endregion
 }
