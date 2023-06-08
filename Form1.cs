b3802eb0d605205d629b1f68822f463f28810332
PaulineFR <pauline.franquelin@gmail.com>
2023-06-08 13:57:41 +0200

    achieve view

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using TaquinPuzzle.Model;
 
 namespace TaquinPuzzle;
@@ -5,6 +6,7 @@ namespace TaquinPuzzle;
 public partial class Form1 : Form
 {
     private Grid _grid { get; set; }
+    private List<Color> colorList = new List<Color>();
 
     public Form1()
     {
@@ -38,8 +40,24 @@ public partial class Form1 : Form
         int lines = (int)numericGridSize.Value;
         float x = 0;
         float y = 0;
-        float xSpace = panelGrid.Width / lines;
-        float ySpace = panelGrid.Height / lines;
+        float xSpace = (float)panelGrid.Width / lines;
+        float ySpace = (float)panelGrid.Height / lines;
+
+        // draw targets
+        _grid.Agents.ForEach(agent =>
+        {
+            agent.Color = getNewRandomColor();
+            graph.FillRectangle(new SolidBrush(agent.Color), agent.Target.X * xSpace, agent.Target.Y * ySpace, (int)xSpace, (int)ySpace);
+
+        });
+
+        // draw agents
+        _grid.Agents.ForEach(agent =>
+        {
+            graph.FillEllipse(new SolidBrush(agent.Color), new Rectangle(agent.Position.X * (int)xSpace, agent.Position.Y * (int)ySpace, (int)xSpace, (int)ySpace));
+            graph.DrawEllipse(penIs, new Rectangle(agent.Position.X * (int)xSpace, agent.Position.Y * (int)ySpace, (int)xSpace, (int)ySpace));
+        });
+
 
         // vertical lines
         for (int i = 0; i < lines + 1; i++)
@@ -57,7 +75,20 @@ public partial class Form1 : Form
             y += ySpace;
 
         }
-        
+
+    }
+    private Color getNewRandomColor()
+    {
+        // Sets a random color for the truck, this function has been extracted to be more readable
+        Random random = new Random();
+        Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        while (this.colorList.Contains(randomColor))
+        {
+            randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+        this.colorList.Add(randomColor);
+
+        return randomColor;
     }
 
     #endregion
--- Model/Agent.cs
+++ Model/Agent.cs
@@ -7,4 +7,6 @@ public class Agent
     public Coordinates Target { get; set; }
     public Grid Grid { get; set; }
     public List<Agent> Others { get { return Grid.Agents.Where(a => a.Id != this.Id).ToList(); } }
+
+    public Color Color { get; set; }
 }
